20e108989ea7748aab8d54423f3c2e27615809fa
atagan-deb <yusei.deb@gmail.com>
2022-03-08 07:07:54 +0900

    postやgetの基礎的な知識

---
+++ 060_PHPとWEB/0500_フォームで配列を送ろう/start/receive.php
@@ -0,0 +1,7 @@
+<?php
+// print_r($_POST['members']);
+
+print_r($_POST['account']);
+$account = $_POST['account'];
+$id = $account['id'];
+echo $id;
---
+++ 060_PHPとWEB/0600_隠しフィールドについて学ぼう/start/index.php
@@ -0,0 +1,14 @@
+
+<form action="post.php" method="POST">
+  <div>
+    個数：<input type="number" name="num" id="">
+  </div>
+  <div>
+    価格：<input type="number" name="price" id="">
+  </div>
+  <div>
+    個数：10%
+  </div>
+  <input type="hidden" name="discount" value="10">
+  <input type="submit" value="合計">
+</form>
